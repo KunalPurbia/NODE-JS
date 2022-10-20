7015b6888d80234dc57eb454b0f392faa505d8f8
KunalPurbia <Kunal.Purbia@softobiz.com>
2022-10-20 15:36:39 +0530

    Changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-}).listen(9000, () => {
-    console.log("Server started on port 9000");
+    res.end('Hello World!!');
+}).listen(5000, () => {
+    console.log("Server started on port 5000");
 })
